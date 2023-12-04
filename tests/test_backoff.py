e983b8961e0b6ed2173c3fe46c939fcb91a586a0
Martijn Sturm <martijn.sturm@gmail.com>
2023-12-04 14:03:50 +0100

    test backoff trackign time in backoff

---
+++ tests/test_backoff.py
@@ -0,0 +1,15 @@
+from data_fetch_optimization.backoff import BackoffManager
+import time
+
+
+def test_backoff_manager_exceeded_max_seconds_in_subsequent_backoff():
+    backoff_manager = BackoffManager(
+        initial_backoff=1,
+        max_backoff=5 * 60,
+        backoff_factor=2,
+        max_seconds_in_subsequent_backoff=10,
+    )
+    backoff_manager.increase_backoff()
+    assert not backoff_manager.has_exceeded_max_time_in_subsequent_backoff()
+    time.sleep(12)
+    assert backoff_manager.has_exceeded_max_time_in_subsequent_backoff()
